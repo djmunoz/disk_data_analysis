826d9e8595960d1abdf48b62bda557edf73ebf05
Diego Munoz <diego.munoz.anguita@gmail.com>
2017-04-11 14:12:30 -0700

    Added HDF5 utilities directly into the package

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from __future__ import print_function
-from distutils.core import setup
+from distutils.core import setup, Extension
 import os
 import sys
+import numpy
+
+# define the extension module
+_cos_module = Extension('cos_module', sources=['c_src/_cos_module.c'])
+_cos_module_np = Extension('cos_module_np', sources=['c_src/_cos_module_np.c'],include_dirs=[numpy.get_include()])
 
 
 setup(
@@ -10,7 +15,8 @@ setup(
     author='Diego J. Munoz',
     author_email = 'diego.munoz.anguita@gmail.com',
     url='https://github.com/',
-    packages=['disk_data_analysis','disk_data_analysis.circumbinary'],
+    packages=['disk_data_analysis','disk_data_analysis.circumbinary','disk_hdf5'],
     description='Analysis and Plotting Tools for HDF5 simulation data of meshless/moving-mesh hydrodynamical disks',
-    install_requires = ['numpy','scipy']
+    install_requires = ['numpy','scipy'],
+    ext_modules=[_cos_module,_cos_module_np]
 )
