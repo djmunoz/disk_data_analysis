52250c269d7a8e27345bcb3650bd1e438564c279
Diego Munoz <diego.munoz.anguita@gmail.com>
2017-04-11 14:39:58 -0700

    Minor fixes to setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ import os
 import sys
 import numpy
 
-# define the extension module
-_cos_module = Extension('cos_module', sources=['c_src/_cos_module.c'])
-_cos_module_np = Extension('cos_module_np', sources=['c_src/_cos_module_np.c'],include_dirs=[numpy.get_include()])
 
 
 setup(
@@ -18,5 +15,5 @@ setup(
     packages=['disk_data_analysis','disk_data_analysis.circumbinary','disk_hdf5'],
     description='Analysis and Plotting Tools for HDF5 simulation data of meshless/moving-mesh hydrodynamical disks',
     install_requires = ['numpy','scipy'],
-    ext_modules=[_cos_module,_cos_module_np]
+#    ext_modules=[]
 )
