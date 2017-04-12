fa8196cc086cbbdb24fc47df77805970780d0d7a
Diego Munoz <diego.munoz.anguita@gmail.com>
2017-04-12 13:25:44 -0700

    Improvement and simplification of snapshot reading functions

--- disk_data_analysis/circumbinary/disk_simulation_data.py
+++ disk_data_analysis/circumbinary/disk_simulation_data.py
@@ -18,43 +18,99 @@ datablocks = {"POS ":["Coordinates",3],
 	      "PRES":["Pressure",1],
               }
 
-def get_snapshot_data(filename_prefix,snap_num,quantities,code="AREPO"):
-    print code,"hehe"
-    print quantities
+
+STAR_PARTTYPE = 4
+
+
+class gas_data():
+    def __init__(self,*args,**kwargs):
+        self.pos=kwargs.get("POS")
+        self.vel=kwargs.get("VEL")
+        self.dens=kwargs.get("RHO")
+        self.utherm=kwargs.get("U")
+        self.ids=kwargs.get("ID")
+
+        self.R = kwargs.get("R")
+        self.phi = kwargs.get("PHI")
+        self.velR = kwargs.get("VELR")
+        self.velphi = kwargs.get("VELPHI")        
+        
+class particle_data():
+    def __init__(self,*args,**kwargs):
+        self.pos=kwargs.get("pos")
+        self.vel=kwargs.get("vel")
+        self.mass=kwargs.get("mass")
+        self.ids=kwargs.get("ids")
+
+        if (self.pos is None):
+            self.pos = np.empty([0,3])
+        if (self.vel is None):
+            self.vel = np.empty([0,3])
+        if (self.mass is None):
+            self.mass = np.empty([0])
+        if (self.ids is None):
+            self.ids = np.empty([0])
+
+
+class snapshot():
+    """
+    Snapshot class, containing -- in a single data structure -- the simulation snapshot information
+
+    """
+    
+    def __init__(self,*args,**kwargs):
+
+        self.header = kwargs.get("header")
+        
+        self.parttype=kwargs.get("parttype")
+        
+        if (self.parttype == 0):
+            self.gas = gas_data(**kwargs)
+        if (self.parttype == 1):
+            self.particle = particle_data(**kwargs)
+        
+                
+
+def get_snapshot_data(filename_prefix,snap_num,quantities,parttype= 0 ,code="AREPO"):
+
     nquant=len(quantities)
     outquant = []
+    if (parttype is None): types = [0,1,2,3,4]
+    else: types = [parttype]
+
     for quant in quantities:
         if (code == "AREPO"):
-            if (quant.ljust(4) in datablocks):
-                outquant.append(rs.read_block(filename_prefix+str(snap_num).zfill(3),quant.ljust(4),parttype=0))
-            elif (quant in disk_data_fields):
-                header = rs.snapshot_header(filename_prefix+snap_num)
-                BoxX,BoxY = header.boxsize,header.boxsize
-                if ((quant == "R") | (quant == "PHI")):
-                    pos = rs.read_block(filename_prefix+snap_num,"POS ", parttype=0)
-                    pos[:,0],pos[:,1] = (pos[:,0]- BoxX/2), (pos[:,1]- BoxY/2)
-                    radius = np.sqrt(pos[:,0]**2 + pos[:,1]**2)
-                    phi = np.arctan2(pos[:,1],pos[:,0])
-                    if (quant == "R"):
-                        outquant.append(radius)
-                    if (quant == "PHI"):
-                        outquant.append(phi)
-                if ((quant == "VELR") | (quant == "VELPHI")):
-                    pos = rs.read_block(filename_prefix+snap_num,"POS ", parttype=0)
-                    vel = rs.read_block(filename_prefix+snap_num,"VEL ", parttype=0)
-                    pos[:,0],pos[:,1] = (pos[:,0]- BoxX/2), (pos[:,1]- BoxY/2)
-                    radius = np.sqrt(pos[:,0]**2 + pos[:,1]**2)
-                    phi = np.arctan2(pos[:,1],pos[:,0])
-                    vphi = -np.sin(phi) * vel[:,0] + np.cos(phi) * vel[:,1]
-                    vr   =  np.cos(phi) * vel[:,0] + np.sin(phi) * vel[:,1]
-                    if (quant == "VELR"):
-                        outquant.append(vr)
-                    if (quant == "VELPHI"):
-                        outquant.append(vphi)
+            header = rs.snapshot_header(filename_prefix+str(snap_num).zfill(3))
+            for parttype in types:
+                if (quant.ljust(4) in datablocks):
+                    outquant.append(rs.read_block(filename_prefix+str(snap_num).zfill(3),quant.ljust(4),parttype=parttype))
+                elif (quant in disk_data_fields):
+                    BoxX,BoxY = header.boxsize,header.boxsize
+                    if ((quant == "R") | (quant == "PHI")):
+                        pos = rs.read_block(filename_prefix+snap_num,"POS ", parttype=parttype)
+                        pos[:,0],pos[:,1] = (pos[:,0]- BoxX/2), (pos[:,1]- BoxY/2)
+                        radius = np.sqrt(pos[:,0]**2 + pos[:,1]**2)
+                        phi = np.arctan2(pos[:,1],pos[:,0])
+                        if (quant == "R"):
+                            outquant.append(radius)
+                        if (quant == "PHI"):
+                            outquant.append(phi)
+                    if ((quant == "VELR") | (quant == "VELPHI")):
+                        pos = rs.read_block(filename_prefix+snap_num,"POS ", parttype=parttype)
+                        vel = rs.read_block(filename_prefix+snap_num,"VEL ", parttype=parttype)
+                        pos[:,0],pos[:,1] = (pos[:,0]- BoxX/2), (pos[:,1]- BoxY/2)
+                        radius = np.sqrt(pos[:,0]**2 + pos[:,1]**2)
+                        phi = np.arctan2(pos[:,1],pos[:,0])
+                        vphi = -np.sin(phi) * vel[:,0] + np.cos(phi) * vel[:,1]
+                        vr   =  np.cos(phi) * vel[:,0] + np.sin(phi) * vel[:,1]
+                        if (quant == "VELR"):
+                            outquant.append(vr)
+                        if (quant == "VELPHI"):
+                            outquant.append(vphi)
 
-            else: 
-                print "[error] Quantity type ", quant, "not known!"
-                sys.exit()  
+                else: 
+                    print "[error] Quantity type ", quant, "not known!"
+                    sys.exit()  
 
         elif (code == "PLUTO"):
             if (quant.ljust(4) in datablocks):
@@ -62,7 +118,6 @@ def get_snapshot_data(filename_prefix,snap_num,quantities,code="AREPO"):
             elif (quant in disk_data_fields):
                 if ((quant == "R") | (quant == "PHI")):
                     pos = rspluto.read_data_block(filename_prefix,snap_num,"POS ")
-                    print pos.shape
                     radius = pos[:,0]
                     phi = pos[:,1]
                     if (quant == "R"):
@@ -85,7 +140,11 @@ def get_snapshot_data(filename_prefix,snap_num,quantities,code="AREPO"):
             print "[error] Simulations code ", code, "not known or supported!"
             sys.exit()  
 
-    return outquant
+
+    attributes = dict(zip(quantities,outquant))
+    snap = snapshot(parttype=parttype,header=header,**attributes)
+    
+    return snap
 
 
 def get_snapshot_time(filename,snapnum,code="AREPO",snapinterval=0):
