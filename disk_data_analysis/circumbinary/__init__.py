e847b973dbce42bc01b4dfb10878fa87af338a60
Diego Munoz <diego.munoz.anguita@gmail.com>
2017-04-24 14:17:01 -0700

    New grid data structure

--- disk_data_analysis/circumbinary/__init__.py
+++ disk_data_analysis/circumbinary/__init__.py
@@ -3,6 +3,6 @@ __all__ = ["get_snapshot_data","get_snapshot_time",
            "disk_interpolate_primitive_quantities"]
 
 from disk_simulation_data import get_snapshot_data, get_snapshot_time
-from disk_interpolate_primitive import disk_interpolate_primitive_quantities
+from disk_interpolate_primitive import grid_polar, disk_interpolate_primitive_quantities
 import compute_profiles
 import plot_profiles
