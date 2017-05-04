caea2a430506cd8678ce6eb666967e1ec3699519
Diego Munoz <diego.munoz.anguita@gmail.com>
2017-05-04 12:20:53 -0700

    updated __init__.py file

--- disk_data_analysis/circumbinary/__init__.py
+++ disk_data_analysis/circumbinary/__init__.py
@@ -1,8 +1,10 @@
 __all__ = ["get_snapshot_data","get_snapshot_time","compute_snapshot_gradient",
            "compute_profiles","plot_profiles",
-           "grid_polar", "grid_cartesian", "disk_interpolate_primitive_quantities","disk_interpolate_gradient_quantities"]
+           "grid_polar", "grid_cartesian", "disk_interpolate_primitive_quantities","disk_interpolate_gradient_quantities",
+           "disk_compute_radial_balance"]
 
 from disk_simulation_data import get_snapshot_data, get_snapshot_time, compute_snapshot_gradient
 from disk_interpolate_primitive import grid_polar, grid_cartesian, disk_interpolate_primitive_quantities, disk_interpolate_gradient_quantities
+from disk_analysis import disk_compute_radial_balance
 import compute_profiles
 import plot_profiles
