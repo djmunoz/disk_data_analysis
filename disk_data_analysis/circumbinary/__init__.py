7e71e785010fdffc4ede200116a31d9ec7c261cc
Diego J. Munoz <diego.munoz.anguita@gmail.com>
2017-10-21 16:59:57 -0700

    Included force computation from simulation ASCII files

--- disk_data_analysis/circumbinary/__init__.py
+++ disk_data_analysis/circumbinary/__init__.py
@@ -4,12 +4,14 @@ __all__ = ["get_snapshot_data","get_snapshot_time","compute_snapshot_gradient",
            "compute_angular_momentum_flux_viscosity","compute_angular_momentum_flux_gravity",
            "compute_profiles","plot_profiles",
            "grid_polar", "grid_cartesian", "disk_interpolate_primitive_quantities","disk_interpolate_gradient_quantities",
-           "disk_compute_radial_balance"]
+           "disk_compute_radial_balance",
+           "create_binary_externalforces_files"]
 
 from disk_simulation_data import get_snapshot_data, get_snapshot_time, compute_snapshot_gradient, compute_external_gravforce_from_snapshot,compute_external_gravforce
 from disk_interpolate_primitive import grid_polar, grid_cartesian, disk_interpolate_primitive_quantities, disk_interpolate_gradient_quantities
 from disk_analysis import disk_compute_radial_balance
 from disk_angular_momentum import compute_angular_momentum_transfer,compute_angular_momentum_flux_gravity,compute_angular_momentum_flux_advection, \
     compute_angular_momentum_flux_viscosity
+from disk_time_series import create_binary_externalforces_files
 import compute_profiles
 import plot_profiles
