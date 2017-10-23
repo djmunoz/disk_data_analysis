e3522c0577dfe7ca6ce5a539246a00582cf5d7fa
Diego J. Munoz <diego.munoz.anguita@gmail.com>
2017-10-23 13:53:17 -0700

    Included force computation from simulation ASCII files

--- disk_data_analysis/circumbinary/__init__.py
+++ disk_data_analysis/circumbinary/__init__.py
@@ -6,7 +6,7 @@ __all__ = ["get_snapshot_data","get_snapshot_time","compute_snapshot_gradient",
            "grid_polar", "grid_cartesian", "disk_interpolate_primitive_quantities","disk_interpolate_gradient_quantities",
            "disk_compute_radial_balance",
            "write_binary_externalforces_file","read_binary_externalforces_file","read_binary_accretion_file","compute_binary_angular_momentum_change",
-           "compute_binary_orbital_change"]
+           "compute_binary_orbital_change","compute_external_torques"]
 
 from disk_simulation_data import get_snapshot_data, get_snapshot_time, compute_snapshot_gradient, compute_external_gravforce_from_snapshot,compute_external_gravforce
 from disk_interpolate_primitive import grid_polar, grid_cartesian, disk_interpolate_primitive_quantities, disk_interpolate_gradient_quantities
@@ -14,7 +14,7 @@ from disk_analysis import disk_compute_radial_balance
 from disk_angular_momentum import compute_angular_momentum_transfer,compute_angular_momentum_flux_gravity,compute_angular_momentum_flux_advection, \
     compute_angular_momentum_flux_viscosity
 from disk_time_series import write_binary_externalforces_file, read_binary_externalforces_file, read_binary_accretion_file, \
-    compute_binary_angular_momentum_change, compute_binary_orbital_change
+    compute_binary_angular_momentum_change, compute_binary_orbital_change ,compute_external_torques
 import compute_profiles
 import plot_profiles
 
